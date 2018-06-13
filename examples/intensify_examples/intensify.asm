a98636de1f9329012258c3680997209005156731
Laeri <lars.wuethrich@hotmail.com>
2018-06-13 18:45:14 +0200

    load palette with library word

--- examples/intensify_examples/intensify.asm
+++ examples/intensify_examples/intensify.asm
@@ -11,13 +11,103 @@
 Start:
   LDA #$FF
   TAX
+begin_while1:
   JSR true
-  JSR set_intensify_greens
+  ASL A
+  BCC end_while1
+  LDA $0200,X
+  INX
+begin_then_while1:
+  DEX
+  STA $0200,X
+  LDA #$03
+  JSR load_var
+  DEX
+  STA $0200,X
+  LDA #$FF
+  JSR equal
+begin_if1:
+  CLC
+  ASL A
+  BCC begin_else1
+begin_then1:
+  LDA $0200,X
+  INX
+  JSR true
+  JSR set_intensify_reds
+  JMP end_if1
+begin_else1:
+  LDA $0200,X
+  INX
+  JSR false
+  JSR set_intensify_reds
+end_if1:
+  DEX
+  STA $0200,X
+  LDA #$02
+  JSR load_var
+  DEX
+  STA $0200,X
+  LDA #$01
+  JSR add
+  JSR dup
+  DEX
+  STA $0200,X
+  LDA #$02
+  JSR store_var
+  DEX
+  STA $0200,X
+  LDA #$FF
+  JSR equal
+begin_if2:
+  CLC
+  ASL A
+  BCC begin_else2
+begin_then2:
+  LDA $0200,X
+  INX
+  DEX
+  STA $0200,X
+  LDA #$00
+  DEX
+  STA $0200,X
+  LDA #$02
+  JSR store_var
+  DEX
+  STA $0200,X
+  LDA #$03
+  JSR load_var
+  DEX
+  STA $0200,X
+  LDA #$01
+  JSR add
+  DEX
+  STA $0200,X
+  LDA #$03
+  JSR store_var
+  JMP end_if2
+begin_else2:
+  LDA $0200,X
+  INX
+end_if2:
+  JMP begin_while1
+end_while1:
+  LDA $0200,X
+  INX
   JMP End
 true:
   DEX
   STA $0200,X
   LDA #$FF
+  RTS
+false:
+  DEX
+  STA $0200,X
+  LDA #$00
+  RTS
+dup:
+  DEX
+  STA $0200,X
   RTS
 and:
   AND $0200,X
@@ -39,12 +129,27 @@ load_var:
   TAY
   LDA $00,Y
   RTS
-set_intensify_greens:
-begin_if1:
+add:
+  CLC
+  ADC $0200,X
+  INX
+  RTS
+equal:
+  CMP $0200,X
+  BEQ equal_true
+  LDA #$00
+  JMP equal_done
+equal_true:
+  LDA #$FF
+equal_done:
+  INX
+  RTS
+set_intensify_reds:
+begin_if3:
   CLC
   ASL A
-  BCC begin_else1
-begin_then1:
+  BCC begin_else3
+begin_then3:
   LDA $0200,X
   INX
   DEX
@@ -53,10 +158,10 @@ begin_then1:
   JSR load_var
   DEX
   STA $0200,X
-  LDA #$40
+  LDA #$20
   JSR or
-  JMP end_if1
-begin_else1:
+  JMP end_if3
+begin_else3:
   LDA $0200,X
   INX
   DEX
@@ -65,9 +170,9 @@ begin_else1:
   JSR load_var
   DEX
   STA $0200,X
-  LDA #$BF
+  LDA #$DF
   JSR and
-end_if1:
+end_if3:
   STA $2001
   DEX
   STA $0200,X
